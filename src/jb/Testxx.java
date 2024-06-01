1263a04ce90f1d22ad35f633c1ea52695551b2cf
zhaojing0224 <ztomirror@gmail.com>
2024-06-01 13:47:39 +0900

    dd
    dd

--- src/jb/Testxx.java
+++ src/jb/Testxx.java
@@ -3,7 +3,7 @@ package jb;
 public class Testxx {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
 
 		System.out.println(123);
 	}
---
+++ src/jb/ch07/Java007.java
@@ -0,0 +1,10 @@
+package jb.ch07;
+
+public class Java007 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
